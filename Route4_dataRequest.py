58ded3fa95432499edab657d223c6db89f437966
Miguel Fernandez <44243326+miguelf88@users.noreply.github.com>
2020-07-27 15:07:02 -0400

    Update Route4_dataRequest.py

--- Route4_dataRequest.py
+++ Route4_dataRequest.py
@@ -1,34 +1,37 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[72]:
+###
+# Due to COVID-19, GoTriangle has put their On-Board Survey on hold.
+# The agency wants to examine ridership weekly to establish a baseline level for ridership
+# When ridership returns to normal or a new normal is identified, the survey will proceed.
+# GoTriangle has requested ridership every week so I wrote this script to automate the process
+# The ridership data is saved with the week in the file name
+# The final file is saved with the day it was written to track updates
+###
 
-
+# Import libraries
 import pandas as pd
 from os import walk
 from datetime import date
 
-
-# In[45]:
-
-
+# Set path to data
 path = r'D:\Project Data\Triangle Data Request\Triangle Request'
 
+# Read in data
 f = []
 for (dirpath, dirnames, filenames) in walk(path):
     for x in filenames:
         if x.startswith('stop'):
             f.append(x)
-
-
-# In[79]:
-
-
+            
+# Create empty lists
 week_of = []
 avg_ridership = []
 route = []
 dayOfWeek = []
 
+# Loop through files and perform calculations
 for filename in f:
     df = pd.read_excel(path + '\\' + filename)  # read in excel file
     file_slice = filename[18:26]  # create object that contains start of week date
@@ -49,24 +52,8 @@ output['Week Of'] = pd.to_datetime(output['Week Of'], format = '%Y%m%d')
 # Remove the time component from datetime object
 output['Week Of'] = output['Week Of'].dt.date
 
-
-# In[80]:
-
-
+# Set today's date
 today = str(date.today())
 
 # Write to csv file
 output.to_excel("D:\Project Data\Triangle Data Request\PART_ridership_" + today + ".xlsx", index = False)
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
